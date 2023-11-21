f4e0eadfdad23499d151b7c8046956f081a5cb0d
thalesaguiar <dev.thalesaguiar@gmail.com>
2023-11-20 23:45:38 -0300

    feat: create new component and remove phosphor icons

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,10 +1,32 @@
 import { Header } from "@components/header";
+import { FlatList } from "react-native";
 import { Container } from "./styles";
+import { useState } from 'react';
+import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
+
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+
   return (
     <Container>
-      <Header/>
+      <Header />
+
+      <Highlight
+        title="Turmas"
+        subtitle="jogue com a sua turma"
+      />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+      />
     </Container>
   );
 }
