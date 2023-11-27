2c68122737ab58d1633b361047aa67f489955dc9
Thales Aguiar <dev.thalesaguiar@gmail.com>
2023-11-27 19:16:26 -0300

    feat: created new component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,6 +3,8 @@ import { Container, Form } from './styles';
 import { Highlight } from '@components/Highlight';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
+import { Filter } from '@components/Filter';
+
 
 export function Players() {
   return (
@@ -23,6 +25,11 @@ export function Players() {
         icon="add"
       />
     </Form>
+
+    <Filter 
+      title='Time A'
+      
+    />
 
     </Container>
   );
