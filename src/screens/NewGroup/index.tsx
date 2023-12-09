2ea1afc07a8a3040f94c4df650afaca6bb05c5e1
Thales Aguiar <dev.thalesaguiar@gmail.com>
2023-12-08 22:53:44 -0300

    feat: create get and create Async function

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,46 +1,44 @@
-import { Header } from '@components/header';
-import { Container, Content, Icon } from './styles';
-import { Highlight } from '@components/Highlight';
-import { Button } from '@components/Button';
-import { Input } from '@components/Input';
-import { useNavigation } from '@react-navigation/native';
-import { useState } from 'react';
+import { Header } from "@components/header";
+import { Container, Content, Icon } from "./styles";
+import { Highlight } from "@components/Highlight";
+import { Button } from "@components/Button";
+import { Input } from "@components/Input";
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
+import { groupCreate } from "@storage/group/goupCreate";
 
 export function NewGroup() {
-
-  const [group, setGroup] = useState('');
+  const [group, setGroup] = useState("");
 
   const navigation = useNavigation();
 
-  function handleNew() {
-    navigation.navigate('players' , { group : group }) /* ou passamos assim {group : group } mas a primeira forma é melhor  */
+  async function handleNew() {
+    try {
+      await groupCreate(group);
+      navigation.navigate("players", {
+        group: group,
+      }); /* ou passamos assim {group : group } mas a primeira forma é melhor  */
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
     <Container>
       <Header showBackButton />
-      
+
       <Content>
         <Icon />
 
         <Highlight
-        title="Nova turma"
-        subtitle="crie uma turma para adicionar as pessoas"
-      />
-
-      <Input
-        placeholder="Nome da turma"
-        onChangeText={setGroup}
+          title="Nova turma"
+          subtitle="crie uma turma para adicionar as pessoas"
         />
 
-      <Button
-          title="Criar"
-          style={{ marginTop: 20 }}
-          onPress={handleNew}
-      /> 
-      </Content>
+        <Input placeholder="Nome da turma" onChangeText={setGroup} />
 
-      
+        <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
+      </Content>
     </Container>
-  )
+  );
 }
--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,51 +1,51 @@
 import { Header } from "@components/header";
 import { FlatList } from "react-native";
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
+
+import { groupsGetAll }
 
 import { Container } from "./styles";
-import { useState } from 'react';
+import { useState } from "react";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
 
-
-export function Groups( ) {
+export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
 
   const navigation = useNavigation();
 
   function handleNewGroup() {
-    navigation.navigate('new');
+    navigation.navigate("new");
+  }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupsGetAll();
+      setGroups(data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
     <Container>
       <Header />
 
-      <Highlight
-        title="Turmas"
-        subtitle="jogue com a sua turma"
-      />
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
 
       <FlatList
         data={groups}
-        keyExtractor={item => item}
-        renderItem={({ item }) => (
-          <GroupCard
-            title={item}
-          />
-        )}
-        contentContainerStyle={groups.length === 0 && {flex: 1}}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => (
           <ListEmpty message="Cadastre a primeira turma" />
         )}
         showsHorizontalScrollIndicator={false}
       />
-      <Button
-        title="Criar nova turma"
-        onPress={handleNewGroup}
-      />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   );
 }
